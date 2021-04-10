905b99b8b7e86a89d21d9bab6f31d49e616b0f34
goofyd <sravankumarmv@outlook.com>
2021-04-10 14:18:26 +0530

    Updated the UI of View Scores

--- app/scoreboard/views.py
+++ app/scoreboard/views.py
@@ -9,7 +9,20 @@ def scorecard(request, id):
     for inning in innings:
         score = {}
         score['runs'] = get_list_or_404(Score.objects.order_by('ball_count'), innings=inning)
+        score['scoreboard'] = get_induvidual_scores(score['runs'])
         score['innings'] = inning
         score['total'] = sum([x.runs for x in score['runs']])
+        score['wkts'] = sum([1 for x in score['runs'] if x.action == 'WICKET'])
+        score['overs'] = max([x.ball_count for x in score['runs']])
         summary.append(score)
     return render(request, 'scoreboard/score.html', { 'match' : match, 'innings': summary})
+
+def get_induvidual_scores(obj):
+    players = {}
+    for o in obj:
+        if o.batsman in players.keys():
+            players[o.batsman] = int(players.get(o.batsman)) + int(o.runs)
+        else:
+            players[o.batsman] = 0
+    print(players)
+    return players
